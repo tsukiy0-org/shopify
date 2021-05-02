3b96e538baf2c6916417be8727c12bf6eee57b73
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-05-03 01:14:11 +1000

    test: imports of shopify-graphql-types

---
+++ packages/import-test/src/index.ts
@@ -0,0 +1,10 @@
+import { Scalars } from "@tsukiy0/shopify-graphql-types/dist/2020-04";
+import { v202004 } from "@tsukiy0/shopify-graphql-types";
+
+export const fromDist = (): Scalars["ID"] => {
+  return "";
+};
+
+export const fromIndex = (): v202004.Scalars["ID"] => {
+  return "";
+};
