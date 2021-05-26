ec11132d9e836f2113c16118e1cad7e7f5e0eab2
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-05-26 22:47:27 +1000

    feat: index has only most recent api version

--- packages/shopify-admin-graphql-types/src/index.ts
+++ packages/shopify-admin-graphql-types/src/index.ts
@@ -1,5 +1,1 @@
-export * as v202004 from "./2020-04";
-export * as v202007 from "./2020-07";
-export * as v202010 from "./2020-10";
-export * as v202101 from "./2021-01";
-export * as v202104 from "./2021-04";
+export * from "./2021-04";
