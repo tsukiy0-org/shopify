79a9d89e4967b48032bd7d7501fa22f6caf68a0e
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-05-29 14:00:23 +1000

    feat: export GetUsageSubscriptionResonse

--- packages/shopify-app-core/src/billing/index.ts
+++ packages/shopify-app-core/src/billing/index.ts
@@ -8,6 +8,7 @@ export * from "./services/IAppUsageSubscriptionService";
 export * from "./handlers/models/CreateUsageSubscriptionRequest";
 export * from "./handlers/models/CreateUsageSubscriptionResponse";
 export * from "./handlers/models/GetUsageSubscriptionRequest";
+export * from "./handlers/models/GetUsageSubscriptionResponse";
 export * from "./handlers/models/UpdateUsageSubscriptionCappedAmountRequest";
 export * from "./handlers/models/UpdateUsageSubscriptionCappedAmountResponse";
 export * from "./handlers/UsageSubscriptionHandler";
