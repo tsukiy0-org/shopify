42269f899f53e53d6789ce6a73aeb8ab6cf44bd3
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-07-17 12:25:42 +1000

    feat: add AuthRouterV2

--- packages/shopify-app-express/src/index.ts
+++ packages/shopify-app-express/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./routers/AuthRouter";
+export * from "./routers/AuthRouterV2";
 export * from "./routers/UsageSubscriptionRouter";
 export * from "./routers/WebhookRouter";
 export * from "./middlewares/JwtAuthMiddleware";
