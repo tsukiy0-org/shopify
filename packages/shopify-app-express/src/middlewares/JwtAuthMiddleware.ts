6352ae4ce682d89249451df25802869882ba1c81
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-07-20 22:17:03 +1000

    fix: JwtAuthMiddleware not calling next

--- packages/shopify-app-express/src/middlewares/JwtAuthMiddleware.ts
+++ packages/shopify-app-express/src/middlewares/JwtAuthMiddleware.ts
@@ -3,6 +3,7 @@ import { ApiSecretKey, ShopId } from "@tsukiy0/shopify-app-core";
 import { RequestHandler } from "express";
 import { RequestVerifier } from "../utils/RequestVerifier";
 import { Response } from "express";
+import { promisifyHandler } from "@tsukiy0/extensions-express";
 
 export class JwtAuthMiddleware {
   private readonly requestVerifier: RequestVerifier;
@@ -12,21 +13,21 @@ export class JwtAuthMiddleware {
     this.requestVerifier = new RequestVerifier(config);
   }
 
-  handler: RequestHandler = (req, res) => {
+  handler: RequestHandler = promisifyHandler(async (req, res) => {
     try {
       const authHeader = req.header("Authorization");
 
       if (!authHeader) {
-        return res.status(401);
+        return res.status(401).end();
       }
 
       const jwt = authHeader.replace("Bearer ", "");
       const shopId = this.requestVerifier.verifyJwt(jwt);
       res.locals[this.key] = ShopId.check(shopId);
     } catch {
-      return res.status(401);
+      return res.status(401).end();
     }
-  };
+  });
 
   getShopId = (res: Response): ShopId => {
     return ShopId.check(res.locals[this.key]);
