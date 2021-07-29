adfb43edf90f62ee7e3be73041c5ca4aca8455d7
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-07-29 21:56:43 +1000

    feat: test JwtAuthMiddleware

---
+++ packages/shopify-app-express/src/middlewares/JwtAuthMiddleware.integrationTest.ts
@@ -0,0 +1,47 @@
+import {
+  SystemConfiguration,
+  Url,
+  UrlExtensions,
+} from "@tsukiy0/extensions-core";
+import { ShopId } from "@tsukiy0/shopify-app-core";
+import { ApiSecretKey } from "@tsukiy0/shopify-app-core";
+import { AuthHelper } from "@tsukiy0/shopify-app-infrastructure-testing";
+import fetch from "cross-fetch";
+
+describe("JwtAuthMiddleware", () => {
+  let apiUrl: Url;
+  let token: string;
+
+  beforeEach(() => {
+    const config = new SystemConfiguration();
+    apiUrl = Url.check(config.get("API_URL"));
+    const shopifyApiSecretKey = ApiSecretKey.check(
+      config.get("SHOPIFY_API_SECRET_KEY"),
+    );
+    const shopId = ShopId.check(config.get("SHOP_ID"));
+    token = AuthHelper.generateJwt(shopId, shopifyApiSecretKey);
+  });
+
+  it("when good token then 200", async () => {
+    const url = UrlExtensions.appendPath(apiUrl, "/private/v1");
+
+    const res = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    expect(res.status).toEqual(200);
+  });
+
+  it("when no token then 401", async () => {
+    const url = UrlExtensions.appendPath(apiUrl, "/private/v1");
+
+    const res = await fetch(url, {
+      method: "GET",
+    });
+
+    expect(res.status).toEqual(401);
+  });
+});
