7c67af211377327f4210b9d11d1fa989d7c704de
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-08-04 22:33:30 +1000

    feat: export VerifyHmacWebhookMiddleware

--- packages/shopify-app-express/src/index.ts
+++ packages/shopify-app-express/src/index.ts
@@ -3,3 +3,4 @@ export * from "./routers/UsageSubscriptionRouter";
 export * from "./routers/WebhookRouter";
 export * from "./middlewares/JwtAuthMiddleware";
 export * from "./middlewares/VerifyHmacQueryMiddleware";
+export * from "./middlewares/VerifyHmacWebhookMiddleware";
